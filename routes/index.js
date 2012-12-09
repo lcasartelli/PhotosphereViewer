9a974d4cd646e95f14ee6f278c40e91a596ef2c3
Luca Casartelli <lc.casartelli@gmail.com>
2012-12-09 21:38:35 +0100

    Fix "code style"

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 
+
 var easyimg = 	require('easyimage');
 var mongoose = 	require('mongoose');
 var fs = 		require('fs');
@@ -11,11 +12,11 @@ var THUMBNAILS_PATH = "/uploads/images/thumbnails/";
 var THUMB_WIDTH = 	128;
 var THUMB_HEIGHT = 	128;
 
-
+//MongoDB init	
 var db = mongoose.createConnection('mongodb://localhost/database', 
-									function(err) { if(err) throw err; });		//MongoDB init
-	
-var PhotoSchema_def = {											//Schema structure
+									function(err) { if(err) throw err; });		
+//Schema structure
+var PhotoSchema_def = {											
     titolo: 		String,
     descrizione: 	String,
     path: 			String,
@@ -23,76 +24,85 @@ var PhotoSchema_def = {											//Schema structure
     thumbnail: 		String
 };
 var photoSchema = new mongoose.Schema(PhotoSchema_def);
-var Photo = db.model('Photo', photoSchema);						//Instance use for all queries
+var Photo = db.model('Photo', photoSchema);				//Instance use for all queries
+	
 
-exports.index = function(req, res){
+//Prende dal db tutte le istanze di Photo e processa il template
+exports.index = function (req, res){						
 
-	//Prende dal db tutte le istanze di Photo e processa il template
+	
 
-	Photo.find().exec(function (err, results) {
-  							if (err) 
+	Photo.find().exec( function (err, results) {
+  							if (err) {
+
   								throw err;
-
+  							}	
   							res.render('index', { title: 'Home',photos: results});
 	});	
 };
 
-exports.viewer = function(req, res) {
 
-	//Ottiene partendo dall'id l'istanza di Photo corrispondente e processa il template
+//Ottiene partendo dall'id l'istanza di Photo corrispondente e processa il template
+exports.viewer = function(req, res) {						
+	
 
 	var photo = Photo.findById(req.params.id).exec(function(err, result) {
 			
-				if (err) 
+				if (err) {
+
 					throw err;
+				}
 		
   				photo ? res.render('detail', { title:result.titolo, descrizione:result.descrizione, image:result.path}) : res.render('404');
 	});
 };
 
 
+//Salva dalla cartella temporanea a /uploads/images l'immagine caricata
 exports.upload = function(req, res) {
 
-	fs.readFile(req.files.image.path, function (err, data) {			//Salva dalla cartella temporanea a /uploads/images l'immagine caricata
+	fs.readFile(req.files.image.path, function (err, data) {			
 
 			fs.writeFile(PUBLIC_PATH+IMAGES_PATH+req.files.image.name, data, function (err) {
 				if(err)	throw err;
  			});
 	});
 
-	function save() {
-		
-		//Memorizza del db foto e metadati associati
+	//Memorizza del db foto e metadati associati
+	var save = function save() {	
 
 		var saved_image = new Photo({titolo: req.body.titolo, descrizione:req.body.descr, path: IMAGES_PATH+req.files.image.name, filename:req.files.image.name, thumbnail: THUMBNAILS_PATH+req.files.image.name});
 		saved_image.save(function (err) {
-  					if (err) return handleError(err);
+  					if (err) {
+
+  						return handleError(err);
+  					}
 		});
 	}
-
+	//Creazione thumbnail
+		
 	easyimg.thumbnail(
-		{
-			//Creazione thumbnail
-			
+		{			
 			src: PUBLIC_PATH+IMAGES_PATH+req.files.image.name, dst:PUBLIC_PATH+THUMBNAILS_PATH+req.files.image.name,
 			width:THUMB_WIDTH, height:THUMB_HEIGHT,
 			x:0, y:0
 		},
 		function(err, image) {
-			if (err) 
-				throw err;
+			if (err) {
+
+				throw err;	
+			}
 
 			console.log('Thumbnail created');
 			console.log(image);
 	
 			save();
-
 			res.redirect('/');
 		}
 	);
 };
 
-exports.add = function(req, res) {
+exports.add = function (req, res) {
 	
 	res.render('add', { title:"Carica immagine"});
 };
